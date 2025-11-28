d9373adf5f8bca29977f52d3215ec72205bc871c
Deullam <deullam@yahoo.com.br>
2025-11-28 03:58:36 -0300

    feat(domain): add credit entity and unit tests

---
+++ server/Deullam.Credit.Inquiry.Challenge.Common.Tests/Features/GerenciarCredito/ObjectMother.Credito.cs
@@ -0,0 +1,30 @@
+﻿// Namespace: Deullam.Credit.Inquiry.Challenge.Common.Tests.Features.GerenciarCredito
+namespace Deullam.Credit.Inquiry.Challenge.Common.Tests.Features.GerenciarCredito
+{
+    using Deullam.Credit.Inquiry.Challenge.Domain.Features.GerenciarCredito;
+    using System;
+
+    public static partial class ObjectMother
+    {
+        /// <summary>
+        /// Cria uma instância padrão de um Crédito para uso em testes.
+        /// </summary>
+        public static Credito GetDefaultCredito()
+        {
+            return new Credito
+            {
+                Id = 1,
+                NumeroCredito = "123456",
+                NumeroNfse = "7891011",
+                DataConstituicao = new DateTime(2024, 2, 25),
+                ValorIssqn = 1500.75m,
+                TipoCredito = "ISSQN",
+                SimplesNacional = true,
+                Aliquota = 5.0m,
+                ValorFaturado = 30000.00m,
+                ValorDeducao = 5000.00m,
+                BaseCalculo = 25000.00m
+            };
+        }
+    }
+}
---
+++ server/Deullam.Credit.Inquiry.Challenge.Domain.Tests/Features/GerenciarCredito/CreditoTests.cs
@@ -0,0 +1,33 @@
+﻿// Namespace: Deullam.Credit.Inquiry.Challenge.Domain.Tests.Features.GerenciarCredito
+namespace Deullam.Credit.Inquiry.Challenge.Domain.Tests.Features.GerenciarCredito
+{
+    // Adicionando o using para o nosso projeto de testes comuns
+    using Deullam.Credit.Inquiry.Challenge.Common.Tests.Features.GerenciarCredito;
+    using Deullam.Credit.Inquiry.Challenge.Domain.Features.GerenciarCredito;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class CreditoTests
+    {
+        [Test(Description = "Deve Criar Crédito Com Sucesso")]
+        [Category("Testes de Unidade - Domínio - Credito")]
+        public void Credito_DeveSerCriadoComSucesso_QuandoDadosValidosForemFornecidos()
+        {
+            //Arrange
+            // A lógica de criação do objeto de teste está centralizada e reutilizável.
+            Credito creditoTemplate = ObjectMother.GetDefaultCredito();
+
+            // Act
+            Credito credito = creditoTemplate;
+
+            // Assert
+            credito.Should().NotBeNull();
+            credito.NumeroCredito.Should().Be(creditoTemplate.NumeroCredito);
+            credito.NumeroNfse.Should().Be(creditoTemplate.NumeroNfse);
+            credito.DataConstituicao.Should().Be(creditoTemplate.DataConstituicao);
+            credito.ValorIssqn.Should().Be(creditoTemplate.ValorIssqn);
+            credito.SimplesNacional.Should().Be(creditoTemplate.SimplesNacional);
+        }
+    }
+}
